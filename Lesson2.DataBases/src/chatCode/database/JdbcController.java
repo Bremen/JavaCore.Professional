52fb9b729235b8fddcacdf33a8e98b422f0c4bbb
Boris Remen <boris.remen@gmail.com>
2019-02-06 07:38:38 +0300

    Сделан первый пункт доп.дз по БД. Восстановление БД. Таблицы создаются в JdbcController в функции createTables.

--- Lesson2.DataBases/src/chatCode/database/JdbcController.java
+++ Lesson2.DataBases/src/chatCode/database/JdbcController.java
@@ -20,8 +20,30 @@ public class JdbcController {
             Class.forName(JDBC_CLASS_NAME);
             connection = DriverManager.getConnection(URL_JDBC);
             statement = connection.createStatement();
+
+            createTables();
+            fillUsersTableTestData();
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void fillUsersTableTestData() {
+        try {
+            //очищаем таблицу перед заполнением
+            statement.execute("DELETE from users");
+
+            //сбрасываем счетчик автоинкрементирования первичного ключа на ноль
+            statement.execute("UPDATE SQLITE_SEQUENCE SET SEQ=0 WHERE NAME='users'");
+
+            for (int i = 0; i < 10; i++) {
+                String query = String.format(
+                        "INSERT INTO users (login, password, nickname) VALUES ('%s', '%s', '%s')", "login"+i,"pass"+i,"nick"+i);
+
+                statement.execute(query);
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -60,4 +82,20 @@ public class JdbcController {
         } catch (SQLException e) {e.printStackTrace();}
     }
 
+    private void createTables(){
+        try {
+            //Создаем таблицу продуктов если она еще не была создана
+            statement.execute("CREATE TABLE IF NOT EXISTS users(" +
+                    "id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE, " +
+                    "login TEXT NOT NULL UNIQUE, " +
+                    "password TEXT NOT NULL, " +
+                    "nickname TEXT NOT NULL UNIQUE)");
+
+            statement.execute("CREATE TABLE IF NOT EXISTS blacklist(" +
+                    "nick_id INTEGER NOT NULL, " +
+                    "blocked_nick_id INTEGER NOT NULL)");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
