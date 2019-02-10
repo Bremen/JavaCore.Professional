740344f0d9122a424842325dbb011a0a580f0484
Boris Remen <boris.remen@gmail.com>
2019-02-10 13:54:29 +0300

    история чата сохраняется в файл chat.history и подгружается последние VIEWCOUNT сообщений

--- Lesson3.IODevices/src/network/ChatUtilizer.java
+++
@@ -1,9 +0,0 @@
-package network;
-
-import java.io.IOException;
-
-public interface ChatUtilizer {
-    String authenticationLoop() throws IOException;
-    void conversationLoop() throws IOException;
-    void commandProcessor(String s) throws IOException;
-}
--- Lesson3.IODevices/src/network/Server/Cleaner.java
+++
@@ -1,5 +0,0 @@
-package network.Server;
-
-public interface Cleaner {
-    void scheduledCleaning();
-}
